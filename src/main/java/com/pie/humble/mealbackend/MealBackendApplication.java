d2770e5df9aa827a40bea521781d2a24bc9143e9
Łukasz Marciniak <Lukasz.Marciniak@tlx.local>
2017-11-01 18:58:48 +0100

    maven project initialization

---
+++ src/main/java/com/pie/humble/mealbackend/MealBackendApplication.java
@@ -0,0 +1,12 @@
+package com.pie.humble.mealbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MealBackendApplication.class, args);
+	}
+}
---
+++ src/test/java/com/pie/humble/mealbackend/MealBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pie.humble.mealbackend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MealBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
